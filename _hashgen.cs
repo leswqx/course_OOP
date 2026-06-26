d5a40d8c5e49d747271186e91e51b24e68cab815
leswqx <uunn2994@gmail.com>
2026-06-26 19:28:19 +0300

    Remove unwanted files (IDE, AI, Docs, _hashgen, configs)

--- _hashgen.cs
+++
@@ -1,4 +0,0 @@
-using BCrypt.Net;
-Console.WriteLine("admin123: " + BCrypt.HashPassword("admin123"));
-Console.WriteLine("realtor123: " + BCrypt.HashPassword("realtor123"));
-Console.WriteLine("client123: " + BCrypt.HashPassword("client123"));
--- _hashgen/Program.cs
+++
@@ -1,5 +0,0 @@
-using System;
-using BCrypt.Net;
-Console.WriteLine(BCrypt.Net.BCrypt.HashPassword("admin123"));
-Console.WriteLine(BCrypt.Net.BCrypt.HashPassword("realtor123"));
-Console.WriteLine(BCrypt.Net.BCrypt.HashPassword("client123"));
